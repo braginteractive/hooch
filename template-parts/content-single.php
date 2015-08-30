88f4af8424dd816caba25c317a63fb3d56aacfd1
Brad Williams <braginteractive@gmail.com>
2015-08-30 12:58:31 -0500

    add featured image to content

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -18,6 +18,14 @@
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
+	<div class="featured-image">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+			<?php the_post_thumbnail(); ?>
+			</a>
+		<?php endif; ?>
+	</div>
+
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php
--- template-parts/content.php
+++ template-parts/content.php
@@ -20,6 +20,14 @@
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
+	<div class="featured-image">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+			<?php the_post_thumbnail(); ?>
+			</a>
+		<?php endif; ?>
+	</div>
+
 	<div class="entry-content">
 		<?php
 			the_content( sprintf(
